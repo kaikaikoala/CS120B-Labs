737267a1b2f6c78b51259ac1966748f6158081fc
JoePark1993 <jpark259@ucr.edu>
2018-07-31 14:53:23 -0700

    naming

--- pkawa001_lab2_part4/pkawa001_lab2_part4/main.c
+++ pkawa001_lab2_part4/pkawa001_lab2_part4/main.c
@@ -1,18 +1,35 @@
-/*
- * pkawa001_lab2_part4.c
- *
- * Created: 7/31/2018 2:27:11 PM
- * Author : ucrcse
- */ 
+/*	Partner(s) Name & E-mail: Philip Kaishin Kawada pkawa001@ucr.edu
+ *	Lab Section: 21
+ *	Assignment: Lab # 2 Exercise # 4
+ *	Exercise Description:
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 
 #include <avr/io.h>
 
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	unsigned char fuel = 0x00;
+	unsigned char condition = 0x00;
+	while (1)
+	{
+		fuel = PINA & 0x0F;
+		condition = (PINA & 0xF0)>>4;
+		
+		PORTC = fuel<<4;
+		PORTB = condition & 0x0F;
+	}
 }
-
