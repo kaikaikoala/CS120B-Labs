7ce286d11460c88ce6e60f31c9758d76ac4c7f6a
JoePark1993 <jpark259@ucr.edu>
2018-08-02 13:12:23 -0700

    lab4

---
+++ jpark259_lab4_part2/jpark259_lab4_part2/main.c
@@ -0,0 +1,105 @@
+
+
+/*
+ * jpark259_lab3_part2.c
+ *
+ * Created: 8/1/2018 1:26:21 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+enum States {init,base,up,down,max,min} myState;
+
+unsigned char PA , cnt=7 ;
+	
+void counter(){
+	//transitions
+	switch(myState){
+		case( init ):
+			myState = base;
+			break;
+		case(base):
+			if( cnt == 9){
+				myState = max;
+			}
+			else if( cnt ==0 || PA==0x03 ){
+				myState = min ;	
+			}
+			
+			else if( PA == 0x01 ){
+				myState = up;
+			}
+			else if( PA == 0x02 ){
+				myState = down;
+			}
+			else{
+				myState = base;
+			}
+			break;
+		case(up):
+			if(PA==0x01){
+				myState=up;
+			}
+			else if(PA==0x03){
+				myState=min;
+			}
+			else if(PA!=0x01){
+				myState=base;
+				++cnt;
+			}
+			break;
+		case(down):
+			if(PA==0x02){
+				myState=down;
+			}
+			else if(PA==0x03){
+				myState=min;
+			}
+			else if(PA!=0x02){
+				myState=base;
+				--cnt;
+			}
+			break;
+		case(max):
+			if(PA==0x03){
+				myState=min;
+			}
+			else if(PA==0x02){
+				myState=down;
+			}
+			else{
+				myState=max;
+			}
+			break;
+		case(min):
+			if(PA==0x01){
+				myState=up;
+			}
+			else{
+				myState=min;
+			}
+			break;
+	}
+	//action
+	switch(myState){
+		case(min):
+			cnt=0;
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	myState=init;
+    /* Replace with your application code */
+    while (1) 
+    {
+		PA = PINA & 0x03;
+		counter();
+		PORTC = cnt ;
+    }
+}
