6120823c9d03537fd24cca0ff83757d1ec0d642c
Philip Kaishin Kawada <pkshots@gmail.com>
2018-08-09 16:30:00 -0700

    Start lab6

---
+++ pkawa001_jpark259_lab6_tester/pkawa001_jpark259_lab6_tester/main.c
@@ -0,0 +1,24 @@
+/*
+ * pkawa001_jpark259_lab6_tester.c
+ *
+ * Created: 8/9/2018 2:29:49 PM
+ * Author : ucrcse
+ */ 
+
+
+#include <avr/io.h>
+#include "io.c"
+
+int main(void)
+{
+	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
+	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
+	
+	// Initializes the LCD display
+	LCD_init();
+	
+	// Starting at position 1 on the LCD screen, writes Hello World
+	LCD_DisplayString(1, "Hello World");
+	
+	while(1) {continue;}
+}
