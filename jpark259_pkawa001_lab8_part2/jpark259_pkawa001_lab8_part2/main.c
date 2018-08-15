016f774655216091e6e37f0145852871da692beb
JoePark1993 <32422763+JoePark1993@users.noreply.github.com>
2018-08-15 12:53:25 -0700

    lab8_part1

---
+++ jpark259_pkawa001_lab8_part2/jpark259_pkawa001_lab8_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * jpark259_pkawa001_lab8_part2.c
+ *
+ * Created: 8/15/2018 12:46:30 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
