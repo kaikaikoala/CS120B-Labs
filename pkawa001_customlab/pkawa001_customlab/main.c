2f5d75ccbfc9f78ea74d75d3eac93846f3df18ab
kaikaikoala <kaishinpk@gmail.com>
2018-08-22 15:11:55 -0700

    Start

---
+++ pkawa001_customlab/pkawa001_customlab/main.c
@@ -0,0 +1,18 @@
+/*
+ * pkawa001_customlab.c
+ *
+ * Created: 8/22/2018 1:48:50 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
